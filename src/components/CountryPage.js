c97d36956b5949d22ab7ab34b023c4044876cd0b
Ana Carolina R Lima <anacarolina.rdel@gmail.com>
2021-08-14 12:45:48 -0300

    CountryPage test2

--- src/components/CountryPage.js
+++ src/components/CountryPage.js
@@ -65,6 +65,8 @@ class CountryPage extends Component {
             
             <div>
                 <Navbar/>
+                {this.state.load?
+                <>
                 <div className="country-page-container">
                     <section className="country-details">
                         <div><img className ="countrypage-img-country"src={this.state.country.flag} alt={`img-${this.state.country.name}`}/></div>
@@ -73,26 +75,28 @@ class CountryPage extends Component {
                         <h3 className="country-page-capital">Capital: {this.state.country.capital}</h3>
                         <h3 className="country-page-capital">Region: {this.state.country.region}</h3>
                         <h3 className="country-page-capital">Area: {this.state.country.area} km<sup>2</sup></h3>
-                        <div className="country-page-list-languages">Language(s): <ul>{this.state.country.languages?this.state.country.languages.map(language=><li>{language.name} </li>):<li>language</li>}</ul></div>
+                        <div className="country-page-list-languages">Language(s): <ul>{this.state.country.languages?this.state.country.languages.map(language=><li key={Math.random()}>{language.name} </li>):<li>language</li>}</ul></div>
                         </article>
                     </section>
-                    <section>
+                </div>
+                    <section className="random-links-cards">
                         <div>
-                            <RandomRecipeCard image={this.state.recipes.strMealThumb} demonym={this.state.recipes.demonym}/>
+                            <RandomRecipeCard image={this.state.recipes.strMealThumb} demonym={this.state.country.demonym} meal={this.state.recipes.strMeal}countryName={this.props.match.params.countryName}/>
                         </div>
                         <div>
-
+                        
                         </div>
-                        <div>
-                            <div>
-                                
-                            </div>
-                            <div>
-                                
-                            </div>
+                        <div>     
+                            
                         </div>
                     </section>
-                </div>
+                    </>
+                    :
+                    <div >
+                        <div className="img-loading-container"><img className="img-loading"src="https://i.pinimg.com/originals/aa/55/60/aa55602cfbce86ac0defd85c9e2672a0.png"alt="Loading"/><h1>Loading...</h1></div>
+                    </div>
+                    }
+                
 
 
             </div>
